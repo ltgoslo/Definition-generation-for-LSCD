958da0abe4f8f5732e3c36e7d4edb8008703a136
MariaFjodorowa <laks.salmon@gmail.com>
2024-02-09 18:31:47 +0100

    median, change point

--- src/bayesian_optimization.py
+++ src/bayesian_optimization.py
@@ -1,4 +1,5 @@
 import argparse
+from statistics import median
 import logging
 import os
 
@@ -7,6 +8,7 @@ from ax.exceptions.core import UserInputError
 import random
 import numpy as np
 import math
+import ruptures as rpt
 import torch
 from sklearn.metrics import accuracy_score
 
@@ -28,7 +30,22 @@ def parse_args():
         required=True,
         help='sense_dis.py results directory.',
     )
+    parser.add_argument(
+        "--method", default="median", choices=(
+            "ax", "median", "change_point",
+        ),
+    )
     return parser.parse_args()
+
+
+def detect_change_point(sequence, n_chp=1):
+    """
+    Detects the indices of change points in a sequence of values
+    """
+    sequence = np.array(sequence)
+    algo = rpt.Dynp(model="rbf", jump=1).fit(sequence)
+    chp_index, length = algo.predict(n_bkps=n_chp)
+    return chp_index
 
 
 def cal_acc(p):
@@ -59,36 +76,43 @@ for metric in METRICS_NAMES:
             encoding="utf8",
     ) as scores_file:
         score = [float(x) for x in scores_file.read().rstrip().split(",") if x]
-    dev_score = [score[x] for x in y_dev_index]
-    logging.info(f"length of truth: {y_true_len}")
-    logging.info(f"length of scores: {len(score)}")
-    assert len(score) == y_true_len
-    # word_ls = ['attack','bag','ball','bit','chairman','circle','contemplation','donkey','edge','face','fiction','gas','graft','head','land','lane','lass','multitude','ounce','part','pin','plane','player','prop','quilt','rag','record','relationship','risk','savage','stab','stroke','thump','tip','tree','twist','word']
 
-    bound_score = [x for x in score if
-                   x != float('inf') and x != float('-inf')]
-    for x in score:
-        if x == float('inf'): bound_score.append(2 * max(bound_score))
-        if x == float('-inf'): bound_score.append(
-            min(bound_score) - abs(min(bound_score)))
-    logging.info(bound_score)
-    try:
-        best_parameters, best_values, _, _ = optimize(
-            parameters=[
-                {"name": "threshold",
-                 "type": "range",
-                 "bounds": [float(min(bound_score)),
-                            float(max(bound_score))], }, ],
-            evaluation_function=cal_acc,
-            minimize=False, random_seed=RANDOM_SEED,
-        )
-    except UserInputError:
-        logging.info(f"impossible to optimize for the metric {metric}")
-        continue
-    logging.info(best_parameters)
-    logging.info(f'best_acc: {cal_acc(best_parameters)}')
+    if args.method == "ax":
+        dev_score = [score[x] for x in y_dev_index]
+        logging.info(f"length of truth: {y_true_len}")
+        logging.info(f"length of scores: {len(score)}")
+        assert len(score) == y_true_len
+        # word_ls = ['attack','bag','ball','bit','chairman','circle','contemplation','donkey','edge','face','fiction','gas','graft','head','land','lane','lass','multitude','ounce','part','pin','plane','player','prop','quilt','rag','record','relationship','risk','savage','stab','stroke','thump','tip','tree','twist','word']
 
-    y_pred = [1 if x > best_parameters['threshold'] else 0 for x in score]
+        bound_score = [x for x in score if
+                       x != float('inf') and x != float('-inf')]
+        for x in score:
+            if x == float('inf'): bound_score.append(2 * max(bound_score))
+            if x == float('-inf'): bound_score.append(
+                min(bound_score) - abs(min(bound_score)))
+        logging.info(bound_score)
+        try:
+            best_parameters, best_values, _, _ = optimize(
+                parameters=[
+                    {"name": "threshold",
+                     "type": "range",
+                     "bounds": [float(min(bound_score)),
+                                float(max(bound_score))], }, ],
+                evaluation_function=cal_acc,
+                minimize=False, random_seed=RANDOM_SEED,
+            )
+        except UserInputError:
+            logging.info(f"impossible to optimize for the metric {metric}")
+            continue
+        logging.info(f"Best parameters: {best_parameters}")
+        logging.info(f'best_acc: {cal_acc(best_parameters)}')
+        threshold = best_parameters['threshold']
+    elif args.method == "median":
+        threshold = median(score)
+    elif args.method == "change_point":
+        threshold = detect_change_point(score)
+
+    y_pred = [1 if x > threshold else 0 for x in score]
     con = []
     for i in range(len(y_pred)):
         if y_pred[i] == 1:
