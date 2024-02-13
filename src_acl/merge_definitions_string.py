7ea4d2c4319872dae8c61b9f9a2b4d59c1444e34
Andrey Kutuzov <akutuzov72@gmail.com>
2024-02-13 21:32:43 +0100

    Full merging

--- src_acl/merge_definitions_string.py
+++ src_acl/merge_definitions_string.py
@@ -7,6 +7,13 @@ from collections import Counter
 import argparse
 import logging
 import os
+from unicodedata import category
+
+
+def normalize(text):
+    text = "".join(ch for ch in text if category(ch)[0] != "P")
+    text = " ".join(text.split())
+    return text
 
 
 if __name__ == "__main__":
@@ -23,6 +30,12 @@ if __name__ == "__main__":
         default="english"
     )
     parser.add_argument(
+        "--punct",
+        type=bool,
+        help="Remove punctuation marks?",
+        default=1
+    )
+    parser.add_argument(
         "--out",
         type=str,
         help="File to save the dataset with merged definitions",
@@ -33,10 +46,16 @@ if __name__ == "__main__":
     os.makedirs(os.path.dirname(args.out), exist_ok=True)
 
     LENGTH = 3  # We merge only definitions longer than LENGTH words
+    DISTANCE = 10  # We merge only definitions with Levenshtein distance less than that
+
     logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
 
     df = pd.read_csv(args.data_path, sep="\t", header=None)
     df.columns = ["word", "usage", "definition"]
+
+    if args.punct:
+        logging.info("Removing punctuation marks from the definitions...")
+        df["definition"] = df["definition"].apply(lambda x: normalize(x))
 
     words = df.word.unique()
 
@@ -47,25 +66,23 @@ if __name__ == "__main__":
         logging.info(f"{word}: {len(definitions)} unique senses")
         # TODO: a better logic for choosing the dominant sense
         # TODO: find all similar pairs
-        source = None
-        for cand in definitions:
-            if len(cand[0].split()) > LENGTH:
-                source = cand[0]
-                break
-        if not source:
+        def2compare = None
+        for source in definitions:
+            if len(source[0].split()) > LENGTH and source[0] not in mappings[word]:
+                def2compare = source[0]
+                for definition in definitions:
+                    definition_text = definition[0]
+                    if definition_text != def2compare and definition_text not in mappings[word]:
+                        distance = levenshtein(definition_text, def2compare)
+                        if distance < DISTANCE:  # Ad-hoc number, just "very similar"
+                            mappings[word][definition_text] = def2compare
+                            logging.debug(f"{definition_text} mapped to {def2compare}")
+        if not def2compare:
             logging.info(f"No dominant sense with a long enough definition found for {word}!")
-        for definition in definitions:
-            definition_text = definition[0]
-            if definition_text != source:
-                #  and len(definition_text.split()) > LENGTH
-                distance = levenshtein(definition_text, source)
-                if distance < 10:  # Ad-hoc number, just "very similar"
-                    mappings[word][definition_text] = source
-        logging.debug(source)
-        logging.debug("=====")
-        for paraphrase in mappings[word]:
-            logging.debug(paraphrase)
+        # for paraphrase in mappings[word]:
+        #    logging.debug(paraphrase)
 
+    logging.info("==================================")
     for word in words:
         new_defs = [
             mappings[word][d] if d in mappings[word] else d
