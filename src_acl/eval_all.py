42d2477e7cf99e428e5d0eb676b5a4841bc07f85
MariaFjodorowa <laks.salmon@gmail.com>
2024-03-27 18:51:06 +0100

    lesk p values

--- src_acl/eval_all.py
+++ src_acl/eval_all.py
@@ -23,7 +23,7 @@ def run(metric, strat_folder, language, f):
             "../src/eval.py",
             "2",
             f"{strat_folder}/defgen/{language}/{metric}_dict.tsv",
-            f"data/{language}/truth/graded.txt",
+            f"../src/data/{language}/truth/graded.txt",
         ], capture_output=True,
     )
     value = result.stdout.decode('utf8').split("\t")
@@ -35,12 +35,39 @@ def run(metric, strat_folder, language, f):
     f.write(f"{out}\n")
 
 
+def run_lesk(strat_folder, language, f):
+    strat = "Lesk without PoS"
+    if "pos_True" in strat_folder:
+        strat = "Lesk with PoS"
+    for metric_dict in glob(f"{strat_folder}/*_dict.tsv"):
+        result = subprocess.run(
+            [
+                sys.executable,
+                "../src/eval.py",
+                "2",
+                metric_dict,
+                f"../src/data/{language}/truth/graded.txt",
+            ], capture_output=True,
+        )
+        value = result.stdout.decode('utf8').split("\t")
+        score, p = value[-2], value[-1].rstrip()
+        metric = os.path.split(metric_dict)[-1].replace("_dict.tsv", "")
+        out = f"{language} {strat} {metric}: score {score} p-value {p}"
+        if float(value[-1]) > 0.05:  # yes this is rough
+            # but probably enough for us
+            out += " too large p-value :("
+        f.write(f"{out}\n")
+
+
 def main():
+    pred_path = "predictions/"
     with open("result.txt", "w", encoding="utf8") as f:
         for language in tqdm(LANGS):
-            for strat_folder in glob(f"merge_results/{language}/*"):
+            for strat_folder in glob(f"{pred_path}merge_results/{language}/*"):
                 run("Cosine", strat_folder, language, f)
                 run("JS", strat_folder, language, f)
+            for lesk_folder in glob(f"{pred_path}lesk/{language}*"):
+                run_lesk(lesk_folder, language, f)
 
 
 if __name__ == '__main__':
