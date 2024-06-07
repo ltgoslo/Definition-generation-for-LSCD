30e3e044072367ec63cdbf51b353f91a9b53b471
MariaFjodorowa <laks.salmon@gmail.com>
2024-06-07 15:10:01 +0200

    leave prompt

--- src_acl/rm_prompts.py
+++ src_acl/rm_prompts.py
@@ -26,7 +26,7 @@ def parse_args():
         "--results_dir",
         default='generated_definitions',
     )
-    parser.add_argument('--rm_whole_prompt', default=True, type=literal_eval)
+    parser.add_argument('--rm_whole_prompt', default=2, type=int, choices=(0, 1, 2))
     return parser.parse_args()
 
 
@@ -42,10 +42,14 @@ if __name__ == '__main__':
     for corpus in glob(f"{args.data_dir}/*.tsv.gz"):
         data = pd.read_csv(corpus, sep='\t', compression='gzip')
         out = os.path.join(res_path, os.path.split(corpus)[-1])
-        if args.rm_whole_prompt:
+        if args.rm_whole_prompt == 0:
             data.drop(data.columns[1], axis=1, inplace=True)
-        else:
+        elif args.rm_whole_prompt == 1:
             data.columns[1] = data.columns[1].apply(
                 lambda x: re.sub(PATTERNS[language], '', x)
+            )
+        else:
+            data.columns[1] = data.columns[1].apply(
+                lambda x: re.search(PATTERNS[language], x).group(0)
             )
         data.to_csv(out, sep='\t', compression='gzip', quoting=quoting)
